8d621545f420f8f91b0a7ca52a33db603a23b9b6
lwinthida <lwinthida161@gmail.com>
2018-06-25 20:22:10 -0700

    Poem

---
+++ python-exercises/ex22.py
@@ -0,0 +1,8 @@
+print("Let's practice everything.")
+print('You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.')
+poem = """
+\t The lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+"""
+print(poem)
